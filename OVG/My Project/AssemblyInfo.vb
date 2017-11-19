07a9e4d04c20adcb3a5db36813ea1f4e25aa12bf
Zeinok <FailureSANZ@gmail.com>
2017-11-19 22:35:58 +0800

    bump version to 0.0.3.0

--- OVG/My Project/AssemblyInfo.vb
+++ OVG/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 指定為預設值:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.2.5")> 
-<Assembly: AssemblyFileVersion("0.0.2.5")> 
+<Assembly: AssemblyVersion("0.0.3.0")> 
+<Assembly: AssemblyFileVersion("0.0.3.0")> 
