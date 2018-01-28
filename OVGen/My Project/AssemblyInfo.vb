bd3c79881272415c662bf78bcb69fc2dbb05393c
Zeinok <FailureSANZ@gmail.com>
2018-01-28 19:54:20 +0800

    bump version to 0.0.11.4

--- OVGen/My Project/AssemblyInfo.vb
+++ OVGen/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 指定為預設值:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.11.3")>
-<Assembly: AssemblyFileVersion("0.0.11.3")>
+<Assembly: AssemblyVersion("0.0.11.4")>
+<Assembly: AssemblyFileVersion("0.0.11.4")>
