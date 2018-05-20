0842f1091fbcfc1cbfc182b1910a27525027bfbc
Zeinok <FailureSANZ@gmail.com>
2018-05-20 21:25:05 +0800

    Improved Peak Speed

--- OVGen/TriggeringAlgorithms.vb
+++ OVGen/TriggeringAlgorithms.vb
@@ -23,55 +23,37 @@
     Function peakSpeedScanning(ByRef wave As WAV, ByVal triggerValue As Integer, ByVal offset As Long, ByVal maxScanLength As Long) As Long
         'OPNA2608: scans how fast a peak is reached centers there
         Dim args As channelOptions = wave.extraArguments
+        Dim REoffset As Integer = risingEdgeTrigger(wave, triggerValue, offset, maxScanLength / 2)
+        offset += REoffset
         peakSpeedScanning = 0
-        Dim firstScan As Long = 0
-        Dim captureLength As Long = 0
-        Dim low As Integer = 128
-        Dim max As Integer = -127
-        Dim peaks As New List(Of Integer)
-        While firstScan < maxScanLength
-            If Math.Floor(wave.getSample(offset + firstScan, True)) < low Then
-                low = Math.Floor(wave.getSample(offset + firstScan, True))
-            End If
-            If Math.Floor(wave.getSample(offset + firstScan, True)) = max Then
-                peaks.Add(firstScan)
-            End If
-            If Math.Floor(wave.getSample(offset + firstScan, True)) > max Then
-                max = Math.Floor(wave.getSample(offset + firstScan, True))
-                peaks.Clear()
-                peaks.Add(firstScan)
-            End If
-            firstScan += 1
-        End While
-        Dim middleDistance As Long = maxScanLength
-        Dim middlePoint As Long = 0
-        For Each peakPoint In peaks
-            Dim currentPoint As Integer = peakPoint
-            While Math.Floor(wave.getSample(offset + currentPoint, True)) > triggerValue And currentPoint >= 0 'find middle point
-                currentPoint -= 1
-            End While
-            Dim distanceToPeak As Long = peakPoint - currentPoint 'distance between middlePoint to peakPoint
-            'If currentPoint < 0 Then
-            '    distanceToPeak = maxScanLength - 1
-            'End If
-            If distanceToPeak < middleDistance And distanceToPeak > 0 Then 'if distance is shortest
-                middlePoint = currentPoint
-                middleDistance = distanceToPeak
-            End If
-        Next
+        Dim peak As Integer = -127
+        Dim distance As ULong = 0
+        Dim shortestDistance As ULong = maxScanLength
+        Dim dy As ULong = 0
+        Dim tempTrigger As ULong = 0
 
-        If middlePoint = -1 Then
-            Dim currentPoint = middlePoint
-            While offset + currentPoint >= 0 And Math.Floor(wave.getSample(offset + currentPoint, True)) > triggerValue And currentPoint >= -maxScanLength
-                currentPoint -= 1
+        While dy < maxScanLength
+            distance = 0
+            tempTrigger = dy
+            While Math.Floor(wave.getSample(offset + dy, True)) >= triggerValue And dy < maxScanLength
+                Dim currentSample As Integer = Math.Floor(wave.getSample(offset + dy, True))
+                If currentSample = peak And distance < shortestDistance Then
+                    peakSpeedScanning = tempTrigger
+                    shortestDistance = distance
+                ElseIf currentSample > peak Then
+                    peak = currentSample
+                    peakSpeedScanning = tempTrigger
+                    shortestDistance = distance
+                End If
+                distance += 1
+                dy += 1
             End While
-            If currentPoint > -maxScanLength Then
-                middlePoint = currentPoint
-            Else
-                middlePoint += 1
-            End If
-        End If
-        peakSpeedScanning = middlePoint
+            While Math.Floor(wave.getSample(offset + dy, True)) < triggerValue And dy < maxScanLength
+                dy += 1
+            End While
+        End While
+        peakSpeedScanning += REoffset
+        'Debug.WriteLine(peakSpeedScanning & "," & maxScanLength)
     End Function
 
     Function lengthScanning(ByRef wave As WAV, ByVal triggerValue As Integer, ByVal offset As Long, ByVal maxScanLength As Long, ByVal scanPositive As Boolean, ByVal scanNegative As Boolean) As Long
